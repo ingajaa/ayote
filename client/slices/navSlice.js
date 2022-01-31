6801466ecd5fa180567b621cbadc7cc008fcd08c
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-01-31 10:04:50 +0000

    feat: starter redux data layer + stack navigation

---
+++ client/slices/navSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null
+};
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestinatin: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    }
+  }
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
+// Selectors, for "grabbing" data
+export const selectOrigin = (state) => {
+  state.nav.origin;
+};
+export const selectDestination = (state) => {
+  state.nav.destination;
+};
+export const selectTravelTimeInformation = (state) => {
+  state.nav.travelTimeInformation;
+};
+
+export default navSlice.reducer;
