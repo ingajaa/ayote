29a729e7adb3bb30d0286b0dad72f533c449dff1
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-02-01 14:33:33 +0000

    feat: eva icon pack

--- client/components/BottomNav.js
+++ client/components/BottomNav.js
@@ -1,19 +1,24 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { Text, BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { Text, BottomNavigation, BottomNavigationTab, Icon } from '@ui-kitten/components';
+
+const DropletIcon = (props) => <Icon {...props} name="droplet-outline" />;
+
+const BellIcon = (props) => <Icon {...props} name="bell-outline" />;
+
+const UserIcon = (props) => <Icon {...props} name="person-outline" />;
 
 const BottomNav = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   return (
     <BottomNavigation selectedIndex={selectedIndex} onSelect={(index) => setSelectedIndex(index)} style={{ width: '100%', position: 'absolute', bottom: 0 }}>
-    <BottomNavigationTab title="USERS" />
-    <BottomNavigationTab title="ORDERS" />
-    <BottomNavigationTab title="TRANSACTIONS" />
-  </BottomNavigation>
+      <BottomNavigationTab icon={DropletIcon} />
+      <BottomNavigationTab icon={BellIcon} />
+      <BottomNavigationTab icon={UserIcon} />
+    </BottomNavigation>
   );
 };
 
 export default BottomNav;
 
 const styles = StyleSheet.create({});
-
--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -5,16 +5,16 @@ import { View, StyleSheet } from 'react-native';
 
 const UserProfile = () => {
   return (
-      <View >
-    <Text style={styles.logoStyle}> 👤 </Text>
+    <View>
+      <Text style={styles.logoStyle}> 👤 </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   logoStyle: {
-    backgroundColor: '#fff',
-  },
-})
+    backgroundColor: '#fff'
+  }
+});
 
 export default UserProfile;
