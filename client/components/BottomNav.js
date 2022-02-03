61b2d820ebc3bb023959912beb4a6f91d0cd0942
Inga Jaanimets <ijaanimets@gmail.com>
2022-02-03 10:39:29 +0200

    refactor: top and bottom navigation

--- client/components/BottomNav.js
+++ client/components/BottomNav.js
@@ -1,6 +1,8 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { Text, BottomNavigation, BottomNavigationTab, Icon } from '@ui-kitten/components';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const DropletIcon = (props) => <Icon {...props} name="droplet-outline" />;
 
@@ -10,9 +12,12 @@ const UserIcon = (props) => <Icon {...props} name="person-outline" />;
 
 const BottomNav = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const navigation = useNavigation();
   return (
     <BottomNavigation selectedIndex={selectedIndex} onSelect={(index) => setSelectedIndex(index)} style={{ width: '100%', position: 'absolute', bottom: 0 }}>
-      <BottomNavigationTab icon={DropletIcon} />
+          <TouchableOpacity style={styles.touchableStyle} onPress={() => navigation.navigate('WaterTrackScreen')}>
+      <BottomNavigationTab style={styles.waterStyle} icon={DropletIcon} />
+      </TouchableOpacity>
       <BottomNavigationTab icon={BellIcon} />
       <BottomNavigationTab icon={UserIcon} />
     </BottomNavigation>
@@ -21,4 +26,15 @@ const BottomNav = () => {
 
 export default BottomNav;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  waterStyle: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  touchableStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    padding: 0
+  }
+});
