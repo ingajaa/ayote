705ece59b45a0160c5ff7b08bf321150ddfd5e41
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-02-06 22:52:35 +0000

    feat: delete meal by id

--- server/controllers/meal.js
+++ server/controllers/meal.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const Meal = require("../models/meal");
+const Meal = require('../models/meal');
 
 module.exports.getAll = async function getAll(req, res) {
   res.send(await Meal.find({}));
@@ -9,4 +9,9 @@ module.exports.getAll = async function getAll(req, res) {
 module.exports.addOne = async function addOne(req, res) {
   const meal = new Meal(req.body);
   res.send(await meal.save());
+};
+
+module.exports.deleteOne = async function deleteOne(req, res) {
+  const { id } = await req.params;
+  res.send(await Meal.deleteOne({ _id: id }));
 };
--- server/router.js
+++ server/router.js
@@ -4,8 +4,12 @@ const router = require('express').Router();
 const Meal = require('./controllers/meal');
 const UserProfile = require('./controllers/userProfile');
 
+// Meal endpoints
 router.get('/meals', Meal.getAll);
 router.post('/meals', Meal.addOne);
+router.delete('/meals/:id', Meal.deleteOne);
+
+// Profile data endpoints
 router.get('/profiles', UserProfile.getAll);
 router.get('/profiles/:id', UserProfile.getById);
 router.post('/profiles', UserProfile.addOne);
