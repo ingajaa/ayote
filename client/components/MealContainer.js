6a0858ca2f298a9a7a5e062234fe76dddd1a991e
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-02-07 11:20:40 +0000

    feat: add modal for meal container

--- client/components/MealContainer.js
+++ client/components/MealContainer.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
-import { Text } from '@ui-kitten/components';
+import { Text, Button, Card, Modal } from '@ui-kitten/components';
 import { useGetAllMealsQuery } from '../services/ayote';
+import { useNavigation } from '@react-navigation/native';
 
 const MealContainer = () => {
+  const navigation = useNavigation();
   const { data, error, isLoading } = useGetAllMealsQuery();
+  const [visible, setVisible] = React.useState(false);
   const renderItem = ({ item }) => {
     return (
       <View style={styles.mealContainerStyle} key={item._id}>
@@ -18,9 +21,33 @@ const MealContainer = () => {
   };
   return (
     <View style={styles.container}>
-      <Text style={styles.label} category="label">
-        Your Meals
-      </Text>
+      <View style={styles.header}>
+        <Text style={styles.label} category="label">
+          Your Meals
+        </Text>
+        <Button style={styles.customMealButton} size="small" status="success" onPress={() => setVisible(true)}>
+          Add
+        </Button>
+        <Modal visible={visible} backdropStyle={styles.backdrop} onBackdropPress={() => setVisible(false)}>
+          <Card disabled={true}>
+            <Button
+              style={styles.modalButton}
+              onPress={() => {
+                setVisible(false);
+                navigation.navigate('SearchResultsScreen');
+              }}
+            >
+              SEARCH ITEM
+            </Button>
+            <Button style={styles.modalButton} onPress={() => setVisible(false)}>
+              ADD CUSTOM MEAL
+            </Button>
+            <Button style={styles.modalButton} onPress={() => setVisible(false)}>
+              SCAN BARCODE
+            </Button>
+          </Card>
+        </Modal>
+      </View>
       {data && data.length > 0 ? <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item._id} /> : <Text style={styles.text}>You are not tracking any meal..</Text>}
     </View>
   );
@@ -29,6 +56,18 @@ const MealContainer = () => {
 const styles = StyleSheet.create({
   container: {
     width: '85%'
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap'
+  },
+  customMealButton: {
+    borderRadius: 100
+  },
+  modalButton: {
+    marginVertical: 5
   },
   mealContainerStyle: {
     backgroundColor: '#fff',
@@ -52,6 +91,9 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 20,
     marginVertical: 10
+  },
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
   }
 });
 
